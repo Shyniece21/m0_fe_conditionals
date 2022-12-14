c6fcfe8517d7ed04a84b13a6a9560bbedd4f326a
Alexis Jones <shyniece21@yahoo.com>
2022-12-13 23:11:06 -0700

    Add updates to comparisons challenge

--- comparisons.js
+++ comparisons.js
@@ -17,31 +17,38 @@ var stringTeachers = "4"
 var numberDogs = 0
 
 // EXAMPLE: log the result of the comparison: is numberTeachers greater than numberStudents?
-console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
+// console.log("Is numberTeachers greater than numberStudents?", numberTeachers > numberStudents);
 // this should log: "Is numberTeachers greater than numberStudents?" false
 
 // YOU DO: log the result of the comparison: is numberTeachers less than numberStudents?
 // this should log: true
+console.log(numberTeachers > numberStudents)
 
 // YOU DO: log the result of the comparison: is numberTeachers strictly equal to stringTeachers?
-console.log(numberTeachers === stringTeachers)
 // this should log: false
+console.log(numberTeachers === stringTeachers)
 
 // YOU DO: log the result of the comparison: is numberTeachers not equal to numberStudents?
 // this should log: true
 
+console.log(numberTeachers <= numberStudents)
+
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 20?
 // this should log: true
+console.log(numberStudents >= 20)
 
 // YOU DO: log the result of the comparison: is numberStudents greater than or equal to 21?
 // this should log: false
+console.log(numberStudents >= 21)
 
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 20?
 // this should log: true
 
+console.log(numberStudents <= 20)
+
 // YOU DO: log the result of the comparison: is numberStudents less than or equal to 21?
 // this should log: true
-
+console.log(numberStudents <= 21)
 
 // #-------------------
 // PART 2: Articulating what you are doing
@@ -54,10 +61,12 @@ console.log(numberTeachers === stringTeachers)
 
 console.log(4 < 9);
 // YOU DO: Explain.
+//This evaluates that four is less than nine. This would log as true.
 
 var books = 3;
 console.log(4 < books);
 // YOU DO: Explain.
+//This explains that 4 is less than the amount of books which is 3. In this case this would log as faulse due to 3 being less than 4.
 
 var friends = 6;
 var siblings = 2;
@@ -68,7 +77,7 @@ var attendees = 9;
 var meals = 8;
 console.log(attendees !== meals);
 // YOU DO: Explain.
-
+// This explains that the number of attendees is not strictly equal to the number of meals. In this case this would log as true because the number of meals shows as 8 which is not equal to the number of meals which shows as 9.
 
 // #-------------------
 // PART 3: Logical Operators
@@ -92,14 +101,14 @@ var age = 1;
 // Determine if the dog loves to play and loves treats
 console.log(lovesToPlay && lovesTreats)
 
-
 // Determine if the dog loves to play and loves the dog park
-
+console.log(lovesToPlay && lovesDogPark)
 
 // Determine if the dog loves to play or loves the dog park
-
+console.log(lovesToPlay || lovesDogPark)
 
 // Determine if the dog loves to play and is a puppy
+console.log(lovesToPlay && age <= 1)
 
 // What did your final line of code evaluate to? Why do you think that is? Explain.
-// ANSWER:
+// ANSWER: My final line of code evaluated as true. This is because both variables met the the condition that both had to be true in order for the condition to be met and log as true.
